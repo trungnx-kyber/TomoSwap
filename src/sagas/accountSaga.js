cf4d5ba8c60d631006c8ad9ad10c0ba4dac357bc
Le Xuan Manh <manhlx.300695@gmail.com>
2019-03-06 16:16:16 +0700

    Add loading token balances with interval (#1)

--- src/sagas/accountSaga.js
+++ src/sagas/accountSaga.js
@@ -1,8 +1,10 @@
+import { delay } from 'redux-saga';
 import { takeLatest, call, put, select } from 'redux-saga/effects';
 import * as accountActions from "../actions/accountAction";
 import { getTokenBalances } from "../services/networkService";
 import { setTokens } from "../actions/tokenAction";
 import { formatBigNumber } from "../utils/helpers";
+import AppConfig from "../config/app";
 import { setWallet, setBalanceLoading } from "../actions/accountAction";
 
 const getTokens = state => state.token.tokens;
@@ -11,11 +13,18 @@ function *importAccount(action) {
   const { address, walletType } = action.payload;
 
   yield put(setWallet(address, walletType));
-  yield call(fetchBalance, address);
+  yield call(fetchBalancesChannel, address);
 }
 
-function *fetchBalance(address) {
-  yield put(setBalanceLoading(true));
+function *fetchBalancesChannel(address) {
+  yield call(fetchBalance, address, true);
+  while (true) {
+    yield call(fetchBalance, address);
+  }
+}
+
+function *fetchBalance(address, isFirstLoading = false) {
+  yield put(setBalanceLoading(isFirstLoading));
 
   const tokens = yield select(getTokens);
   const balances = yield call(getTokenBalances, tokens, address);
@@ -26,6 +35,7 @@ function *fetchBalance(address) {
 
   yield put(setTokens(tokens));
   yield put(setBalanceLoading(false));
+  yield call(delay, AppConfig.BALANCE_FETCHING_INTERVAL);
 }
 
 export default function* accountWatcher() {
