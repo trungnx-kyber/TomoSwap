0eae3dae440003371dfc7e6123ddd2cf9b618b46
trungnx <trung@kyber.network>
2019-03-12 10:00:10 +0700

    Change smart contract

--- src/config/tokens.js
+++ src/config/tokens.js
@@ -18,8 +18,6 @@ export const TOKENS = [
     "symbol": "CTT",
     "name": "ChainTEX Token",
     "address": "0x06cca536f531ac3426077ca39b629d901c5cf272",
-    "liquidityConversionRate": "0xb791bfb3c161ff3018e3633a749ca09b1c3c55b9",
-    "reserveAMM": "0x30b17b7e0ef60e0344961de1b87ec1620145c4e1",
     "decimals": 18,
     "logo": "ctc.png"
   },
@@ -27,8 +25,6 @@ export const TOKENS = [
     "symbol": "KNC",
     "name": "Kyber Network Crystal",
     "address": "0x26d22feb6419d08cc38a10812a231926fe427491",
-    "liquidityConversionRate": "0xbd479b9c9525f0b5216b3250a4a4d64aa3568969",
-    "reserveAMM": "0x87d7c1017e6aa0ef6eb7c4d7ef0d70e98a6a915a",
     "decimals": 16,
     "logo": "knc.svg"
   },
